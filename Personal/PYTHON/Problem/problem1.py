d3f5c41d61b027cacc927fa4e0b926b529555028
Kuldeep Tanwar <kuldeep@Kuldeeps-MacBook-Pro.local>
2025-08-27 21:19:15 +0530

    un-necessary files removed

--- Personal/PYTHON/Problem/problem1.py
+++
@@ -1,4 +0,0 @@
-print('''abc
-adfdfd
-dffdfdf
-dfdfdsf''')
--- Personal/PYTHON/Problem/problem2.py
+++
@@ -1,7 +0,0 @@
-import os
-
-folder_path = "/Users/kuldeep/Desktop/PYTHON"
-files = os.listdir(folder_path)
-
-for file in files:
-    print(file)
